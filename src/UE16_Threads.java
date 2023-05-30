a6630f91a0c2faf6c21710e407e094a100958eff
kataszolloosi <kataszolloosi@gmail.com>
2023-05-30 10:42:13 +0200

    3005

---
+++ src/UE16_Threads.java
@@ -0,0 +1,7 @@
+public class UE16_Threads {
+    public static void main(String[] args) {
+        System.out.println("This code is running in a thread. Main Thread: ");
+        System.out.println("I'm inside the thread: '" + Thread.currentThread().getName() + "' ");
+    }
+
+}
